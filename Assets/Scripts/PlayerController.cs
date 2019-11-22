73a71d0e5aa86dbe7d6a063596cdc853baf1a7c0
Guilhem PECH <guilhem.pech@gmail.com>
2019-11-22 11:57:04 +0100

    :construction: Early WIP light that follow the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,20 +11,32 @@ public class PlayerController : MonoBehaviour
     // Configuration fields
     [SerializeField]
     private float force = 1;
+    [SerializeField]
+    private float smoothTime = 0.1F;
     
+    [SerializeField]
+    private GameObject projectorLight;
     
     // Start is called before the first frame update
     private Vector2 _inputDir;
     private Rigidbody2D _rigidbody2D;
+    private Vector3 _projectorVelocity = Vector3.zero;
 
     private void Start()
     {
+        projectorLight = Instantiate(projectorLight,transform.position,Quaternion.identity);
+        
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     public void OnMovements(InputAction.CallbackContext context)
     {
        _inputDir = context.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        projectorLight.transform.position = Vector3.SmoothDamp(projectorLight.transform.position, transform.position, ref _projectorVelocity, smoothTime);
     }
 
     private void FixedUpdate()

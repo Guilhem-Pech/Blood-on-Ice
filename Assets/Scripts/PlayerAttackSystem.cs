ea7bc78665f5458e02006f7f5a27857aa7456af0
StarSlash <genma23@hotmail.fr>
2019-11-28 14:37:14 +0100

    :sparkles: Add player health and a little of attack system

---
+++ Assets/Scripts/PlayerAttackSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackSystem : MonoBehaviour
+{
+    /// <summary>
+    /// Trigger the front attack of the player
+    /// </summary>
+    public void frontAttack()
+    {
+
+    }
+
+    /// <summary>
+    /// Trigger the Area Of Effect attack of the player
+    /// </summary>
+    public void AOEAttack()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerHealthSystem.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthSystem : MonoBehaviour
+{
+    /// <summary>
+    /// The player current amount of health
+    /// </summary>
+                        private int currentHealth;
+    /// <summary>
+    /// The player maximum amount of health
+    /// </summary>
+    [SerializeField]    private int maxHealth;
+
+    /// <summary>
+    /// Returns the current health of a player
+    /// </summary>
+    /// <returns>The current health of a player</returns>
+    public int getCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    /// <summary>
+    /// Returns the maximum health of a player
+    /// </summary>
+    /// <returns>The maximum health of a player</returns>
+    public int getMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    /// <summary>
+    /// Decreases a player's health by [damage]
+    /// </summary>
+    /// <param name="damage">The amount of health to remove of the player</param>
+    public void takeDamage(int damage)
+    {
+        
+        currentHealth -= damage;
+        if(currentHealth < 0)
+        {
+            this.getKilled();
+        }
+    }
+
+    /// <summary>
+    /// Kills a player
+    /// </summary>
+    /// <returns>The overkill amount</returns>
+    public int getKilled()
+    {
+        //Kill the player here
+        return Mathf.Abs(this.currentHealth);
+    }
+}

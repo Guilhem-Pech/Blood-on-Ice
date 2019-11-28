f1e2026a5b03efdf76e51917b9229bacb5bade3f
Guilhem PECH <guilhem.pech@gmail.com>
2019-11-28 11:15:18 +0100

    :tada: Creating GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private HashSet<GameObject> _playerSet;
+    
+    private static GameManager _instance;
+    
+    public GameManager GetInstance()
+    {
+        return _instance; 
+    }
+
+    public void RegisterPlayer(GameObject playerObject)
+    {
+        if(!_playerSet.Add(playerObject))
+            Debug.LogWarning("Trying to register a Player who's already registered !",this);
+    }
+
+    public void RemovePlayer(GameObject playerObject)
+    {
+        _playerSet.Remove(playerObject);
+    }
+
+    public HashSet<GameObject> GetPlayers()
+    {
+        return _playerSet;
+    }
+    
+    
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        _instance = this;
+        DontDestroyOnLoad( this.gameObject );
+    }
+    
+    
+}

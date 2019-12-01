50ff771a747ccd4f291ed8961f84f84b67072ee0
Guilhem PECH <guilhem.pech@gmail.com>
2019-12-01 18:04:51 +0100

    :construction: Begin holes

---
+++ Assets/Scripts/PlayerHoles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHoles : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3[] vertexPos;
+    private TrailRenderer _trailRenderer;
+
+    private void Start()
+    {
+        _trailRenderer = GetComponent<TrailRenderer>();
+    }
+
+    private void Update()
+    {
+        Vector3[] linepos = new Vector3[_trailRenderer.positionCount];
+        _trailRenderer.GetPositions(linepos);
+        vertexPos = linepos;
+    }
+}

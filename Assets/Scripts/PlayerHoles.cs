2f82e822aff79e0ef7a9f98b5f65767ad755c715
Guilhem PECH <guilhem.pech@gmail.com>
2019-12-01 18:47:29 +0100

    :construction: Hole trail WIP

--- Assets/Scripts/PlayerHoles.cs
+++ Assets/Scripts/PlayerHoles.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHoles : MonoBehaviour
@@ -8,9 +6,11 @@ public class PlayerHoles : MonoBehaviour
     [SerializeField]
     private Vector3[] vertexPos;
     private TrailRenderer _trailRenderer;
+    private EdgeCollider2D _edgeCollider2D;
 
     private void Start()
     {
+        _edgeCollider2D = GetComponent<EdgeCollider2D>();
         _trailRenderer = GetComponent<TrailRenderer>();
     }
 
@@ -19,5 +19,15 @@ public class PlayerHoles : MonoBehaviour
         Vector3[] linepos = new Vector3[_trailRenderer.positionCount];
         _trailRenderer.GetPositions(linepos);
         vertexPos = linepos;
+
+        if (linepos.Length < 5) return;
+        Vector2[] colliderPoints =  new Vector2[linepos.Length-5];
+        for (int i = 0; i < linepos.Length - 5 ; i++)
+        {
+            colliderPoints[i] = transform.InverseTransformPoint(linepos[i]);
+        }
+        
+        _edgeCollider2D.points = colliderPoints;
+       
     }
 }

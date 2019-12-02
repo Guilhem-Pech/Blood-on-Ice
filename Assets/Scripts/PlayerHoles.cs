dfcbb9c7783fc706a60ea53e135068d569ab7165
Guilhem PECH <guilhem.pech@gmail.com>
2019-12-02 15:51:07 +0100

    :construction: Mesh holes

--- Assets/Scripts/PlayerHoles.cs
+++ Assets/Scripts/PlayerHoles.cs
@@ -43,13 +43,49 @@ public class PlayerHoles : MonoBehaviour
         vPos.RemoveRange(0,findIndex);
         vPos[vPos.Count - 1] = vPos[0];
         
-        GameObject hole = Instantiate(holePrefab, transform.position, Quaternion.identity);
-        LineRenderer line = hole.GetComponent<LineRenderer>();
-        line.positionCount = vPos.Count;
-        line.SetPositions(vPos.ToArray());
+        GameObject hole = Instantiate(holePrefab, Vector3.zero, Quaternion.identity);
+        MeshFilter mesh = hole.GetComponent<MeshFilter>();
+      
+        vPos.Add(AvgVector(vPos));
+        mesh.mesh.SetVertices(vPos);
+        //mesh.mesh.SetTriangles(CreateTriangles(vPos),0);
+        mesh.mesh.triangles = CreateTriangles(vPos).ToArray();
         _trailRenderer.Clear();
       
     }
+
+    private List<int> CreateTriangles(List<Vector3> vertex)
+    {
+        int originPoint = vertex.Count - 1;
+        List<int> result = new List<int>();
+        for (int i = 0; i < originPoint; i++)
+        {
+            result.Add((i+1) % originPoint );
+            result.Add(i);
+            result.Add(originPoint);
+            
+            result.Add((i+1) % originPoint );
+            result.Add(originPoint);
+            result.Add(i);
+            
+        }
+        return result;
+    }
+
+    private static Vector2 AvgVector(List<Vector3> vertex)
+    {
+        Vector2 avg = Vector2.zero;
+        foreach (Vector3 v in vertex)
+        {
+            avg.x += v.x;
+            avg.y += v.y;
+        }
+
+        avg.x /= vertex.Count;
+        avg.y /= vertex.Count;
+        return avg;
+    }
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {

b5da51ca3e18576419006b1a35d3bf14c94b9f6a
Guilhem PECH <guilhem.pech@gmail.com>
2019-12-02 23:32:42 +0100

    :sparkles: :construction: Add spotlight test

---
+++ Assets/Scripts/FreeformLight.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace DefaultNamespace
+{
+    [ExecuteAlways]
+    public class FreeformLight : MonoBehaviour
+    {
+        private Light2D _light2D;
+        
+        [SerializeField]
+        private Transform anchor1;
+        [SerializeField]
+        private Transform anchor2; 
+            
+        private Vector3[] shape;
+
+        private void Start()
+        {
+            _light2D = GetComponent<Light2D>();
+            anchor1 = GameManager.GetInstance().spotLightAnchor1;
+            anchor2 = GameManager.GetInstance().spotLightAnchor2;
+        }
+
+        private void Update()
+        {
+            shape = _light2D.shapePath;
+            shape[6] = transform.InverseTransformPoint(anchor1.position);
+            shape[7] = transform.InverseTransformPoint(anchor2.position);
+        }
+    }
+}

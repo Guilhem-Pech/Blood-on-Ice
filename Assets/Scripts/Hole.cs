3d23c5703a0baded8c87690b030e307adccdf6b9
Guilhem PECH <guilhem.pech@gmail.com>
2019-12-04 10:46:26 +0100

    :hankey: Some test with holes

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    public float destructionTime = 5f;
+    private MeshFilter _meshFilter;
+    private void Start()
+    {
+        _meshFilter = GetComponent<MeshFilter>();
+
+        if (_meshFilter.mesh.bounds.size.magnitude < 1)
+            destructionTime = 0f;
+        
+        Destroy(gameObject,destructionTime);
+    }
+}
--- Assets/Scripts/PlayerHoles.cs
+++ Assets/Scripts/PlayerHoles.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 using Unity.Mathematics;
 using UnityEngine;
@@ -39,7 +41,7 @@ public class PlayerHoles : MonoBehaviour
     {
 
         List<Vector3> vPos = new List<Vector3>(vertexPos);
-        int findIndex = vPos.FindIndex(x => Mathf.Abs(Vector2.Distance(x,pos)) <= 0.1);
+        int findIndex = FindIndex(pos, vPos);
         vPos.RemoveRange(0,findIndex);
         vPos[vPos.Count - 1] = vPos[0];
         
@@ -48,16 +50,25 @@ public class PlayerHoles : MonoBehaviour
       
         vPos.Add(AvgVector(vPos));
         mesh.mesh.SetVertices(vPos);
-        //mesh.mesh.SetTriangles(CreateTriangles(vPos),0);
         mesh.mesh.triangles = CreateTriangles(vPos).ToArray();
-        hole.GetComponent<EdgeCollider2D>().points = vec3ToVec2(vPos.ToArray());
+        hole.GetComponent<EdgeCollider2D>().points = Vec3ToVec2(vPos.ToArray());
         _trailRenderer.Clear();
       
     }
 
+    private static int FindIndex(Vector3 pos, List<Vector3> vPos, float defDist = 0.05f , float maxDist = 0.2f)
+    {
+        int findIndex =  vPos.FindIndex(x => Mathf.Abs(Vector2.Distance(x, pos)) <= defDist);
+        while (findIndex == -1 && defDist <= maxDist)
+        {
+            defDist += 0.05f;
+            findIndex = vPos.FindIndex(x => Mathf.Abs(Vector2.Distance(x, pos)) <= defDist);
+        }
+        return findIndex;
+    }
 
 
-    private static Vector2[] vec3ToVec2(IReadOnlyList<Vector3> tab)
+    private static Vector2[] Vec3ToVec2(IReadOnlyList<Vector3> tab)
     {
         Vector2[] result = new Vector2[tab.Count];
         for (int i = 0; i < tab.Count; i++)
@@ -68,7 +79,7 @@ public class PlayerHoles : MonoBehaviour
         return result;
     }
     
-    private List<int> CreateTriangles(List<Vector3> vertex)
+    private static List<int> CreateTriangles(ICollection vertex)
     {
         int originPoint = vertex.Count - 1;
         List<int> result = new List<int>(6*originPoint);
@@ -85,7 +96,7 @@ public class PlayerHoles : MonoBehaviour
         return result;
     }
 
-    private static Vector2 AvgVector(List<Vector3> vertex)
+    private static Vector2 AvgVector(IReadOnlyCollection<Vector3> vertex)
     {
         Vector2 avg = Vector2.zero;
         foreach (Vector3 v in vertex)

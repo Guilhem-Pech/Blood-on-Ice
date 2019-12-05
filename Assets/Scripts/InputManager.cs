8e8c5e78c74cd6d848969fc8332db641cd6fab77
StarSlash <genma23@hotmail.fr>
2019-12-05 09:19:29 +0100

    Merge branch 'master' of https://github.com/Guilhem-Pech/Blood-on-Ice
    Conflicts:
    	Assets/Prefabs/dummy_guy.prefab

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInputManager _playerInputManager;
+
+    private void Start()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,16 +17,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private GameObject projectorLight;
     
-    // Start is called before the first frame update
+    [SerializeField]
+    private Vector3 projectorOffset = Vector3.zero;
+    
     private Vector2 _inputDir;
     private Rigidbody2D _rigidbody2D;
-    private Vector3 _projectorVelocity = Vector3.zero;
+    private Vector2 _projectorVelocity = Vector2.zero;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
     private void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = true;
         GameManager.GetInstance().RegisterPlayer(gameObject);
         gameObject.layer = 9;
         projectorLight = Instantiate(projectorLight,transform.position,Quaternion.identity);
-        
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
@@ -37,7 +42,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        projectorLight.transform.position = Vector3.SmoothDamp(projectorLight.transform.position, transform.position, ref _projectorVelocity, smoothTime);
+        projectorLight.transform.position = Vector2.SmoothDamp(projectorLight.transform.position, transform.position + projectorOffset, ref _projectorVelocity, smoothTime);
     }
 
     private void FixedUpdate()

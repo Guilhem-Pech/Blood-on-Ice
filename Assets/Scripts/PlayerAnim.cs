e2c01c83fdd03cb2f20b03cde0122ab5edfbb42e
Guilhem PECH <guilhem.pech@gmail.com>
2019-12-11 14:25:54 +0100

    :speaker: Integrate Deplacement

---
+++ Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    
+    public void PlayFootstep()
+    {
+       // AkSoundengine.PostEvent(myEvent, gameObject)    
+       AkSoundEngine.PostEvent("SFX_Deplacements", gameObject);
+    }
+}
+

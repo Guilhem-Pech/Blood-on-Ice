1501c574d805545eeaff8f144f43e2668d8fbd55
Guilhem PECH <guilhem.pech@gmail.com>
2019-12-14 21:37:03 +0100

    :art: push

---
+++ Assets/Scripts/Echap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Echap : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
